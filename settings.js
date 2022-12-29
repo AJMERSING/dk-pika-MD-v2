23d30b324307e81e4f85520379a8ced4d6065af5
AJMERSING <121504072+AJMERSING@users.noreply.github.com>
2022-12-29 18:49:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ global.simpleBotname = "Anya" //⚠️ Only use normal and one work name here or
 
              //--------Owner & vcard--------\\
              global.botname = "Queen Anya" 
-             global.ownernumber = ['918811074852'] // Owner number //
-             global.vcardowner = ['918811074852'] // vcard owner number //
-             global.ownername = "3.69_Pika" // Owner name //
+             global.ownernumber = ['918955953704'] // Owner number //
+             global.vcardowner = ['918955953704'] // vcard owner number //
+             global.ownername = "DK-BOSS" // Owner name //
              global.ytname = "YT: ONYXPIKA" // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4" // Github or insta name //
              global.location = "India, Assam, Dibrugarh" // Location //
